3386b6018699691312511352e7e21ba9e79bd377
v81d <v81d@proton.me>
2026-05-30 19:00:59 -0400

    feat(filters): return empty data when ids is empty

--- src/mmk_panel/apps/collectibles/filters.py
+++ src/mmk_panel/apps/collectibles/filters.py
@@ -3,30 +3,34 @@ from django_filters import FilterSet, BaseInFilter, NumberFilter
 from .models import Card, Move, Rarity
 
 
-# Filter by list of IDs (i.e., `?ids=0,1,2,3` to retrieve IDs 0, 1, 2, and 3)
+# Filter by list of IDs (e.g.., `?ids=0,1,2,3` to retrieve IDs 0, 1, 2, and 3)
 class NumberInFilter(BaseInFilter, NumberFilter):
     pass
 
 
-class CardFilterSet(FilterSet):
+class BaseIdFilterSet(FilterSet):
     ids = NumberInFilter(field_name="id", lookup_expr="in")
 
+    def filter_queryset(self, queryset):
+        if not (self.data or {}).get("ids"):
+            return queryset.none()
+
+        return super().filter_queryset(queryset)
+
+
+class CardFilterSet(BaseIdFilterSet):
     class Meta:
         model = Card
         fields = ["name", "nickname", "rarity"]
 
 
-class MoveFilterSet(FilterSet):
-    ids = NumberInFilter(field_name="id", lookup_expr="in")
-
+class MoveFilterSet(BaseIdFilterSet):
     class Meta:
         model = Move
         fields = ["name"]
 
 
-class RarityFilterSet(FilterSet):
-    ids = NumberInFilter(field_name="id", lookup_expr="in")
-
+class RarityFilterSet(BaseIdFilterSet):
     class Meta:
         model = Rarity
         fields = "__all__"
